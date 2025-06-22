a762a57ffd6bf8b7b2173007ec2e84517c7b1755
DuongNA <duongna@ltsgroup.tech>
2025-06-22 21:31:34 +0700

    Remove unused colors

--- lib/gen/colors.gen.dart
+++ lib/gen/colors.gen.dart
@@ -13,63 +13,12 @@ import 'package:flutter/material.dart';
 class ColorName {
   ColorName._();
 
-  /// Color: #F9FAFB
-  static const Color background = Color(0xFFF9FAFB);
-
-  /// Color: #DBEAFE
-  static const Color blueBackground = Color(0xFFDBEAFE);
-
-  /// Color: #2563EB
-  static const Color blueDarkBackground = Color(0xFF2563EB);
-
-  /// Color: #D1D5DB
-  static const Color borderBlack = Color(0xFFD1D5DB);
-
-  /// Color: #D1FAE5
-  static const Color greenBackground = Color(0xFFD1FAE5);
-
-  /// Color: #059669
-  static const Color greenDarkBackground = Color(0xFF059669);
-
-  /// Color: #374151
-  static const Color iconGray = Color(0xFF374151);
-
   /// Color: #141522
   static const Color loginBackground = Color(0xFF141522);
 
   /// Color: #6EE7B7
   static const Color loginGradientEnd = Color(0xFF6EE7B7);
 
-  /// Color: #6B52FF
-  static const Color loginGradientMid1 = Color(0xFF6B52FF);
-
-  /// Color: #6E9AD6
-  static const Color loginGradientMid2 = Color(0xFF6E9AD6);
-
   /// Color: #6840FF
   static const Color loginGradientStart = Color(0xFF6840FF);
-
-  /// Color: #DAD2FF
-  static const Color purpleBackground = Color(0xFFDAD2FF);
-
-  /// Color: #7C3AED
-  static const Color purpleDarkBackground = Color(0xFF7C3AED);
-
-  /// Color: #000000
-  static const Color textBlack = Color(0xFF000000);
-
-  /// Color: #6B7280
-  static const Color textGray = Color(0xFF6B7280);
-
-  /// Color: #059669
-  static const Color textGreen = Color(0xFF059669);
-
-  /// Color: #FFFFFF
-  static const Color white = Color(0xFFFFFFFF);
-
-  /// Color: #DEE0EFFF
-  static const Color whiteLightShadow = Color(0xDEE0EFFF);
-
-  /// Color: #C3C5C9
-  static const Color whiteShadow = Color(0xFFC3C5C9);
 }
